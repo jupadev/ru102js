8e558bb7046d82b0b68cd78854f7a6a08c639780
Juan Pablo Vargas <jupavar@gmail.com>
2023-02-04 12:34:08 -0300

    feat: add findAll

--- src/daos/impl/redis/site_dao_redis_impl.js
+++ src/daos/impl/redis/site_dao_redis_impl.js
@@ -1,5 +1,5 @@
-const redis = require('./redis_client');
-const keyGenerator = require('./redis_key_generator');
+const redis = require("./redis_client");
+const keyGenerator = require("./redis_key_generator");
 
 /**
  * Takes a flat key/value pairs object representing a Redis hash, and
@@ -20,7 +20,7 @@ const remap = (siteHash) => {
   remappedSiteHash.capacity = parseFloat(siteHash.capacity, 10);
 
   // coordinate is optional.
-  if (siteHash.hasOwnProperty('lat') && siteHash.hasOwnProperty('lng')) {
+  if (siteHash.hasOwnProperty("lat") && siteHash.hasOwnProperty("lng")) {
     remappedSiteHash.coordinate = {
       lat: parseFloat(siteHash.lat),
       lng: parseFloat(siteHash.lng),
@@ -46,7 +46,7 @@ const remap = (siteHash) => {
 const flatten = (site) => {
   const flattenedSite = { ...site };
 
-  if (flattenedSite.hasOwnProperty('coordinate')) {
+  if (flattenedSite.hasOwnProperty("coordinate")) {
     flattenedSite.lat = flattenedSite.coordinate.lat;
     flattenedSite.lng = flattenedSite.coordinate.lng;
     delete flattenedSite.coordinate;
@@ -85,7 +85,7 @@ const findById = async (id) => {
 
   const siteHash = await client.hgetallAsync(siteKey);
 
-  return (siteHash === null ? siteHash : remap(siteHash));
+  return siteHash === null ? siteHash : remap(siteHash);
 };
 
 /* eslint-disable arrow-body-style */
@@ -95,8 +95,19 @@ const findById = async (id) => {
  * @returns {Promise} - a Promise, resolving to an array of site objects.
  */
 const findAll = async () => {
-  // START CHALLENGE #1
-  return [];
+  const client = redis.getClient();
+
+  const siteKeys = keyGenerator.getSiteIDsKey();
+  const allIds = await client.smembersAsync(siteKeys);
+
+  const sitePromises = allIds.map((siteKey) => {
+    return client.hgetallAsync(siteKey);
+  });
+
+  const rawSites = await Promise.all(sitePromises);
+  const sites = rawSites.map((data) => remap(data));
+
+  return sites;
   // END CHALLENGE #1
 };
 /* eslint-enable */
